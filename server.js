8fc2831fb4ca69c6bc7d2b4aeb6991066bd812c2
Nathan Dalal <nathanhdalal@gmail.com>
2015-07-29 08:03:41 -0700

    starting sql squeries

--- server.js
+++ server.js
@@ -1,38 +1,36 @@
-var port = 8080;
+var querystring = "";
+var port = 7777;
 var fs = require('fs');
 
 //msyql setup
 var mysql = require('mysql');
 
-//create mysql connection
-var connection = mysql.createConnection({
+//create mysql pool
+var pool = mysql.createPool({
     host     : 'canaryctr-donald.stanford.edu',
     user     : 'markerville_user',
     password : 'b1omark3rsRock!',
-    database : 'information_schema'
+    database : 'MARKERVILLE',
+    multipleStatements: true
 });
 
-connection.connect(function(err) {
+var NUM_BIOMARKERS_SERVER = 10;
+var NUM_DISEASES_SERVER = 10;
+var NUM_USERS_SERVER = 0;
+
+querystring = "SELECT COUNT(*) FROM Biomolecules;" +
+                "SELECT COUNT(*) FROM Diseases;" +
+                "SELECT COUNT(*) FROM Users;";
+pool.query(querystring, function(err, rows, fields) {
     if (err) {
-        console.error('error connecting: ' + err.stack);
+        throw err;
         return;
     }
 
-    console.log('connected as id ' + connection.threadId);
+    NUM_BIOMARKERS_SERVER = rows[0][0]["COUNT(*)"];
+    NUM_DISEASES_SERVER = rows[1][0]["COUNT(*)"];
+    NUM_USERS_SERVER = rows[2][0]["COUNT(*)"];
 });
-
-/*
-var querystring = "SELECT * FROM CHARACTER_SETS;";
-
-connection.query(querystring, function(err, rows, fields) {
-    if (err) 
-        throw err;
- 
-    for (var i in rows) {
-        console.log(rows[i]);
-    }
-});
-*/
 
 //express setup
 var express = require('express');
@@ -55,12 +53,63 @@ app.use(express.static('static'));
 app.get('/', function (request, response) {
     console.log('got a GET request from index');
     var biomarkerName = request.query['biomarker'];
-    if (biomarkerName == undefined)
+    if (biomarkerName == undefined) {
         response.render('pages/index', {
-            search_failed: false
+            search_failed: false,
+            num_biomarkers: NUM_BIOMARKERS_SERVER,
+            num_diseases: NUM_DISEASES_SERVER,
+            num_users: NUM_USERS_SERVER
         });
+    }
     else {
         biomarkerName = biomarkerName.toUpperCase();
+        var medium = "";
+        var type = "";
+
+        //MySQL Dynamic Read
+        querystring = "SELECT fk_Biomolecules FROM Biomolecule_Names WHERE Name=?;";
+        pool.query(querystring, [biomarkerName], function(err, rows, fields) {
+            if (err) {
+                throw err;
+                return;
+            }
+
+            if( !(typeof rows != "undefined" && rows != null && rows.length > 0) ) {
+                response.render('pages/index', {
+                    search_failed: true,
+                    biomarkerName: biomarkerName,
+                    num_biomarkers: NUM_BIOMARKERS_SERVER,
+                    num_diseases: NUM_DISEASES_SERVER,
+                    num_users: NUM_USERS_SERVER
+                });
+            }
+            else {
+                querystring = "SELECT * from Biomolecules WHERE pk_Biomolecules=?;";
+                pool.query(querystring, [rows[0].fk_Biomolecules], function(err, rows, fields) {
+                    if (err) {
+                        throw err;
+                        return;
+                    }
+
+                    console.log(rows[0]);
+                    querystring = "SELECT Medium from Biomolecule_Medium WHERE pk_Biomolecule_Medium=?; " +
+                                    "SELECT Type from Biomolecule_Type WHERE pk_Biomolecule_Type=?;";
+                    pool.query(querystring, [rows[0].fk_Biomolecule_Medium, rows[0].fk_Biomolecule_Type], function(err, rows, fields) {
+                        if (err) {
+                            throw err;
+                            return;
+                        }
+
+                        medium = rows[0][0]["Medium"];
+                        type = rows[1][0]["Type"];
+                        console.log("medium: " + medium + "type: " + type);
+                    });
+                });
+            }
+        });
+
+
+        /* JSON STATIC READ
         var biomarkerJSON = JSON.parse(fs.readFileSync("static/temp_files/biomarker_sample.json", "utf8"));
 
         var biomarkerList = [];
@@ -77,9 +126,13 @@ app.get('/', function (request, response) {
         else {
             response.render('pages/index', {
                 search_failed: true,
-                biomarkerName: biomarkerName
+                biomarkerName: biomarkerName,
+                num_biomarkers: NUM_BIOMARKERS_SERVER,
+                num_diseases: NUM_DISEASES_SERVER,
+                num_users: NUM_USERS_SERVER
             });
         }
+        */
     }
 });
 
@@ -96,6 +149,18 @@ app.get("/login", function (request, response) {
 });
 app.post('/login', function (request, response) {
     console.log("got a login POST request");
+    if (true) {
+        response.render('pages/login', {
+            login_problem_text: "Account not found. Please try again."
+        });
+    }
+    else {
+        response.render('pages/index', {
+            num_biomarkers: NUM_BIOMARKERS_SERVER,
+            num_diseases: NUM_DISEASES_SERVER,
+            num_users: NUM_USERS_SERVER
+        });
+    }
 });
 
 //routing signup
@@ -104,13 +169,19 @@ app.get('/signup', function (request, response) {
     console.log('got a GET request from signup');
 });
 app.post('/signup', function (request, response) {
+    console.log("got a signup POST request");
     if (request.body.user.pwd !== request.body.user.repeatpwd) {
         response.render('pages/signup', {
             create_account_problem_text: "Passwords did not match. Please try again."
         });
     }
+    else if (request.body.user.pwd.length < 5) {
+        response.render('pages/signup', {
+            create_account_problem_text: "Password must contain at least 5 characters. Please try again."
+        });
+    }
     else {
-        response.render('pages/account_creation_success', {
+        response.render('pages/account_creation/success', {
             email: request.body.user.email
         });
     }
@@ -129,10 +200,11 @@ app.use(function (error, request, response, next) {
     console.log("Internal server error: \n" + error);
 });
 
-
 //listen on localhost
 app.listen(port);
 console.log('listening on http://localhost:' + port);
 
-//end mysql connection
-connection.end();
+//end mysql pool
+process.on('exit', function(code) {
+    pool.end();
+});
---
+++ static/js/accordion.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#accordion section h1").click(function(e) {
+    $(this).parents().siblings("section").addClass("ac_hidden");
+    $(this).parents("section").removeClass("ac_hidden");
+
+    e.preventDefault();
+  });
+});
