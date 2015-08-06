7f49cf0bac2050049af2c347dedf3d1a1c38569d
Nathan Dalal <nathanhdalal@gmail.com>
2015-08-05 22:28:29 -0700

    fixed dossier

---
+++ static/js/editing_ux.js
@@ -0,0 +1,36 @@
+function appendName(li_element_id) {
+    var listElement = document.createElement('li');
+
+    var headerspan = document.createElement('span');
+    headerspan.style.fontWeight = 'bold';
+    headerspan.innerHTML = "<br>Another Name: &nbsp;&nbsp;";
+
+    var newspan = document.createElement('span');
+    newspan.innerHTML = "<input type='text' name='biomarkerNames[]'>&nbsp;";
+
+    var buttons_span = document.createElement('span');
+    buttons_span.innerHTML = "<input class='small_button' type='button' " + 
+                                "onclick='appendName" + '("name_inputs");' + "'value='+'>&nbsp;" +
+                                "<input class='small_button' type='button' " + 
+                                "onclick='removeName" + '(this);' + "'value='-'>";
+
+    listElement.appendChild(headerspan);
+    listElement.appendChild(newspan);
+    listElement.appendChild(buttons_span);
+    document.getElementById(li_element_id).appendChild(listElement);
+}
+
+function removeName(removeButtonNode) {
+    var removingSpan = removeButtonNode.parentNode.parentNode;
+    removingSpan.parentNode.removeChild(removingSpan);
+}
+
+function showSubmit() {
+    document.getElementById('editing-submit-button').style.display = 'inline-block';
+    document.getElementById('alternate-submit-button-text').style.display = 'none';
+}
+
+function hideSubmit() {
+    document.getElementById('editing-submit-button').style.display = 'none';
+    document.getElementById('alternate-submit-button-text').style.display = 'inline-block';
+} 
